01fcf9202b07c9975aafc32e68e0a8b53d035126
taylorb2 <99695459+taylorb2@users.noreply.github.com>
2022-04-18 15:22:19 -0400

    Update buffer.h

--- buffer.h
+++ buffer.h
@@ -1,33 +1,40 @@
-#ifndef BUFFER_H
-#define BUFFER_H
+#ifndef BUFFER_SEM_H
+#define BUFFER_SEM_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#ifndef __KERNEL__
+#include <semaphore.h>
+#else
+#include <linux/semaphore.h>
+#endif
+
 #define SIZE_OF_BUFFER 20
+#define DATA_LENGTH 1024
 
-typedef struct node_421 {
-	struct node_421 *next;
-	int data;
-} node_421_t;
+typedef struct bb_node_421 {
+    struct bb_node_421 *next;
+    char data[DATA_LENGTH];
+} bb_node_421_t;
 
-typedef struct ring_buffer_421 {
-   int length;
-   node_421_t *read;
-   node_421_t *write;
-} ring_buffer_421_t;
-
-
+typedef struct bb_buffer_421 {
+    int length;
+    bb_node_421_t *read;
+    bb_node_421_t *write;
+} bb_buffer_421_t;
 
 #ifndef __KERNEL__
 long init_buffer_421(void);
-long insert_buffer_421(int i);
-long print_buffer_421(void);
+long enqueue_buffer_421(char *data);
+long dequeue_buffer_421(char *data);
 long delete_buffer_421(void);
+void print_semaphores(void);
 #endif
 
 #ifdef __cplusplus
 }
 #endif
+
 #endif
